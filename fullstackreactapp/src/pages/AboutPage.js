2220c5742c4801d5e2620dc632eb77175318c555
RyanVickers <55283378+RyanVickers@users.noreply.github.com>
2021-10-17 23:08:35 -0400

    initial Commit

---
+++ fullstackreactapp/src/pages/AboutPage.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const AboutPage = () => (
+    <>
+        <h1>About Me</h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut eu elit facilisis, ornare augue eu, porta dolor. Aenean sapien urna, pulvinar non ligula sed, ullamcorper sodales neque. Sed vulputate eu quam nec dignissim. Nam finibus, orci ac volutpat ultrices, libero felis venenatis massa, id suscipit ex tellus et nibh. Ut sit amet consequat ligula, sit amet iaculis ipsum. Sed porta, felis in laoreet convallis, risus nulla tincidunt augue, vel sagittis tortor ex vitae massa. Duis sit amet arcu et eros mollis iaculis. Cras non diam sed turpis congue vestibulum. Nullam nec nunc odio. Sed a auctor magna, non tristique quam. Quisque ornare enim ac justo egestas, vel pulvinar dolor laoreet.</p>
+        <p>Aliquam sed sollicitudin est, ac vehicula sem. Phasellus id tellus sem. Integer ullamcorper viverra lacus rutrum tincidunt. Fusce vitae dignissim massa. Praesent mauris urna, iaculis eget posuere ut, scelerisque eu nibh. Pellentesque pharetra et eros in finibus. Fusce vel felis a tortor volutpat ornare ac at nisl. Curabitur id mi ac eros sodales accumsan id quis dui. Nullam orci nibh, tempor eu risus in, elementum placerat lectus. Nullam hendrerit fermentum diam at condimentum. In pulvinar aliquet accumsan. Donec nec libero ipsum. Sed eu mi et arcu pretium iaculis.</p>
+    </>
+)
+
+export default AboutPage
